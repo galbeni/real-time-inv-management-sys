f791ec799de46e7fd26fe5c9e174a461b16520a1
Gál Beni <beniauthor@gmail.com>
2025-02-27 23:41:50 +0100

    Install SVG Sprite module and add svg files & Update nuxt config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    pageTransition: {
+      name: 'page',
+      mode: 'out-in'
+    },
+    layoutTransition: {
+      name: 'layout',
+      mode: 'out-in'
+    }
+  },
   compatibilityDate: '2025-02-26',
   devtools: {
     enabled: true
   },
   modules: [
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/svg-sprite'
   ]
 })
