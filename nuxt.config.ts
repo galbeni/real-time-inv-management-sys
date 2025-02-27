51bc8663260dceb17dba439200d9e77f21e4b829
Gál Beni <beniauthor@gmail.com>
2025-02-27 01:24:43 +0100

    Install tailwind css module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-02-26',
-  devtools: { enabled: true }
+  devtools: {
+    enabled: true
+  },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ]
 })
