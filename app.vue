8b6829cc4a6b2c932a62cbe060d2afe78df76104
Gál Beni <beniauthor@gmail.com>
2025-03-01 15:55:50 +0100

    Add app layout and transitions with SCSS styling

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <nuxt-layout>
+      <nuxt-page />
+    </nuxt-layout>
   </div>
 </template>
+
+<style lang="scss">
+  .component-enter-active,
+  .component-leave-active {
+    transition: all 1s ease;
+  }
+
+  .component-enter-from,
+  .component-leave-to {
+    transform: translateY(30px);
+    opacity: 0;
+  }
+</style>
