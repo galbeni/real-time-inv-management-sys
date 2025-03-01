11d358b09445317f714bce1d9411c9218899831c
Gál Beni <beniauthor@gmail.com>
2025-03-01 17:19:18 +0100

    Add date formatting global plugin for consistent date display

---
+++ plugins/dateFormat.ts
@@ -0,0 +1,16 @@
+export default defineNuxtPlugin(() => {
+  return {
+    provide: {
+      formatDate: (date: string | number | Date) => {
+        return new Date(date).toLocaleDateString('en-US', {
+          year: 'numeric',
+          month: 'short',
+          day: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false
+        });
+      }
+    }
+  };
+});
